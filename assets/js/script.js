b5e0c09ca476b132d558b7c9ac694cc56db66c3d
DarioConiglione <dariocng13@gmail.com>
2025-10-28 12:02:29 +0100

    create hover con polaroid

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,29 +7,38 @@ axios.get('https://lanciweb.github.io/demo/api/pictures/')
             arrayEl.push(element)
             console.log(arrayEl);
 
+
             if (arrayEl.length === 6) {
-                for (let i = 0; i = arrayEl.length; i++) {
+                
+                
+                for (let i = 0; i < arrayEl.length; i++) {
+                                        
                     const elementEl = document.createElement("div");
                     elementEl.classList.add("col-sm-12", "col-md-5", "col-lg-3", "mb-2", "polaroid")
 
                     elementEl.innerHTML = `    
 
-                <div class="pin">
-                    <img src="./assets/img/pin.svg" alt="">
-                </div>
+                                            <div class="pin">
+                                                <img src="./assets/img/pin.svg" alt="">
+                                            </div>
 
-                <div class="photo">
-                    <img src="${arrayEl[i].url}" alt="">
-                </div>
+                                            <div class="photo">
+                                                <img src="${arrayEl[i].url}" alt="">
+                                            </div>
 
-                <div class="text">
-                    <p>${arrayEl[i].date}</p>
-                    <p>${arrayEl[i].title}</p>
-                </div>
-                `}
+                                            <div class="text">
+                                                <p>${arrayEl[i].date}</p>
+                                                <p>${arrayEl[i].title}</p>
+                                            </div>
+                                            `
+
+                    secEl.appendChild(elementEl);
+
+                }
 
 
             }
+
 
         });
     })
@@ -37,7 +46,22 @@ axios.get('https://lanciweb.github.io/demo/api/pictures/')
         console.error(error)
     })
 
-const arrayEl = []
+const arrayEl = [];
+
+const secEl = document.getElementById("sec");
+
+const overlayEL = getElementById("overlay")
+
+
+
+// generare un ciclo for per tutta l'array di oggetti
+// aggiungo un addEventListener ad elementEL
+// la funzione delle evento deve rendere l'overlay block ed appendere l'immagine ad overlay-img 
+// aggiungere un addEventListener al elemento bottone per tornare indietro.
+
+
+
+
 
 
 
